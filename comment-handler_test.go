071d9a01c079e2f58ff872eb58233b760b729d0e
Miek Gieben <miek@miek.nl>
2018-05-05 15:15:39 +0100

    Remove underscores from test

---
+++ comment-handler_test.go
@@ -0,0 +1,399 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/miekg/dreck/types"
+)
+
+var actionOptions = []struct {
+	title          string
+	body           string
+	expectedAction string
+}{
+	{
+		title:          "Correct reopen command",
+		body:           Trigger + "reopen",
+		expectedAction: reopenConst,
+	},
+	{ //this case replaces Test_Parsing_Close
+		title:          "Correct close command",
+		body:           Trigger + "close",
+		expectedAction: closeConst,
+	},
+	{
+		title:          "invalid command",
+		body:           Trigger + "dance",
+		expectedAction: "",
+	},
+	{
+		title:          "Longer reopen command",
+		body:           Trigger + "reopen: ",
+		expectedAction: reopenConst,
+	},
+	{
+		title:          "Longer close command",
+		body:           Trigger + "close: ",
+		expectedAction: closeConst,
+	},
+}
+
+func TestParsingOpenClose(t *testing.T) {
+
+	for _, test := range actionOptions {
+		t.Run(test.title, func(t *testing.T) {
+
+			action := parse(test.body)
+
+			if action.Type != test.expectedAction {
+				t.Errorf("Action - want: %s, got %s", test.expectedAction, action.Type)
+			}
+
+		})
+	}
+}
+
+func TestParsingLabels(t *testing.T) {
+
+	var labelOptions = []struct {
+		title        string
+		body         string
+		expectedType string
+		expectedVal  string
+	}{
+		{
+			title:        "Add label of demo",
+			body:         Trigger + "add label: demo",
+			expectedType: "AddLabel",
+			expectedVal:  "demo",
+		},
+		{
+			title:        "Remove label of demo",
+			body:         Trigger + "remove label: demo",
+			expectedType: "RemoveLabel",
+			expectedVal:  "demo",
+		},
+		{
+			title:        "Invalid label action",
+			body:         Trigger + "peel label: demo",
+			expectedType: "",
+			expectedVal:  "",
+		},
+	}
+
+	for _, test := range labelOptions {
+		t.Run(test.title, func(t *testing.T) {
+
+			action := parse(test.body)
+			if action.Type != test.expectedType || action.Value != test.expectedVal {
+				t.Errorf("Action - wanted: %s, got %s\nLabel - wanted: %s, got %s", test.expectedType, action.Type, test.expectedVal, action.Value)
+			}
+		})
+	}
+}
+
+func TestParsingAssignments(t *testing.T) {
+
+	var assignmentOptions = []struct {
+		title        string
+		body         string
+		expectedType string
+		expectedVal  string
+	}{
+		{
+			title:        "Assign to burt",
+			body:         Trigger + "assign: burt",
+			expectedType: assignConst,
+			expectedVal:  "burt",
+		},
+		{
+			title:        "Unassign burt",
+			body:         Trigger + "unassign: burt",
+			expectedType: unassignConst,
+			expectedVal:  "burt",
+		},
+		{
+			title:        "Assign to me",
+			body:         Trigger + "assign: me",
+			expectedType: assignConst,
+			expectedVal:  "me",
+		},
+		{
+			title:        "Unassign me",
+			body:         Trigger + "unassign: me",
+			expectedType: unassignConst,
+			expectedVal:  "me",
+		},
+		{
+			title:        "Invalid assignment action",
+			body:         Trigger + "consign: burt",
+			expectedType: "",
+			expectedVal:  "",
+		},
+		{
+			title:        "Unassign blank",
+			body:         Trigger + "unassign: ",
+			expectedType: "",
+			expectedVal:  "",
+		},
+	}
+
+	for _, test := range assignmentOptions {
+		t.Run(test.title, func(t *testing.T) {
+
+			action := parse(test.body)
+			if action.Type != test.expectedType || action.Value != test.expectedVal {
+				t.Errorf("Action - wanted: %s, got %s\nMaintainer - wanted: %s, got %s", test.expectedType, action.Type, test.expectedVal, action.Value)
+			}
+		})
+	}
+}
+
+func TestParsingTitles(t *testing.T) {
+
+	var titleOptions = []struct {
+		title        string
+		body         string
+		expectedType string
+		expectedVal  string
+	}{
+		{
+			title:        "Set Title",
+			body:         Trigger + "set title: This is a really great Title!",
+			expectedType: setTitleConst,
+			expectedVal:  "This is a really great Title!",
+		},
+		{
+			title:        "Mis-spelling of title",
+			body:         Trigger + "set titel: This is a really great Title!",
+			expectedType: "",
+			expectedVal:  "",
+		},
+		{
+			title:        "Empty Title",
+			body:         Trigger + "set title: ",
+			expectedType: "", //blank because it should fail isValidCommand
+			expectedVal:  "",
+		},
+		{
+			title:        "Empty Title (Double Space)",
+			body:         Trigger + "set title:  ",
+			expectedType: setTitleConst,
+			expectedVal:  "",
+		},
+	}
+
+	for _, test := range titleOptions {
+		t.Run(test.title, func(t *testing.T) {
+
+			action := parse(test.body)
+			if action.Type != test.expectedType || action.Value != test.expectedVal {
+				t.Errorf("\nAction - wanted: %s, got %s\nValue - wanted: %s, got %s", test.expectedType, action.Type, test.expectedVal, action.Value)
+			}
+		})
+	}
+}
+
+func TestAssessState(t *testing.T) {
+
+	var stateOptions = []struct {
+		title            string
+		requestedAction  string
+		currentState     string
+		expectedNewState string
+		expectedBool     bool
+	}{
+		{
+			title:            "Currently Closed and trying to close",
+			requestedAction:  closeConst,
+			currentState:     closedConst,
+			expectedNewState: "",
+			expectedBool:     false,
+		},
+		{
+			title:            "Currently Open and trying to reopen",
+			requestedAction:  reopenConst,
+			currentState:     openConst,
+			expectedNewState: "",
+			expectedBool:     false,
+		},
+		{
+			title:            "Currently Closed and trying to open",
+			requestedAction:  reopenConst,
+			currentState:     closedConst,
+			expectedNewState: openConst,
+			expectedBool:     true,
+		},
+		{
+			title:            "Currently Open and trying to close",
+			requestedAction:  closeConst,
+			currentState:     openConst,
+			expectedNewState: closedConst,
+			expectedBool:     true,
+		},
+	}
+
+	for _, test := range stateOptions {
+		t.Run(test.title, func(t *testing.T) {
+
+			newState, validTransition := checkTransition(test.requestedAction, test.currentState)
+
+			if newState != test.expectedNewState || validTransition != test.expectedBool {
+				t.Errorf("\nStates - wanted: %s, got %s\nValidity - wanted: %t, got %t\n", test.expectedNewState, newState, test.expectedBool, validTransition)
+			}
+		})
+	}
+}
+
+func TestValidAction(t *testing.T) {
+
+	var stateOptions = []struct {
+		title           string
+		running         bool
+		requestedAction string
+		start           string
+		stop            string
+		expectedBool    bool
+	}{
+		{
+			title:           "Currently unlocked and trying to lock",
+			running:         false,
+			requestedAction: lockConst,
+			start:           lockConst,
+			stop:            unlockConst,
+			expectedBool:    true,
+		},
+		{
+			title:           "Currently unlocked and trying to unlock",
+			running:         false,
+			requestedAction: unlockConst,
+			start:           lockConst,
+			stop:            unlockConst,
+			expectedBool:    false,
+		},
+		{
+			title:           "Currently locked and trying to lock",
+			running:         true,
+			requestedAction: lockConst,
+			start:           lockConst,
+			stop:            unlockConst,
+			expectedBool:    false,
+		},
+		{
+			title:           "Currently locked and trying to unlock",
+			running:         true,
+			requestedAction: unlockConst,
+			start:           lockConst,
+			stop:            unlockConst,
+			expectedBool:    true,
+		},
+	}
+
+	for _, test := range stateOptions {
+		t.Run(test.title, func(t *testing.T) {
+
+			isValid := validAction(test.running, test.requestedAction, test.start, test.stop)
+
+			if isValid != test.expectedBool {
+				t.Errorf("\nActions - wanted: %t, got %t\n", test.expectedBool, isValid)
+			}
+		})
+	}
+}
+
+func TestFindLabel(t *testing.T) {
+
+	var stateOptions = []struct {
+		title         string
+		currentLabels []types.IssueLabel
+		cmdLabel      string
+		expectedFound bool
+	}{
+		{
+			title: "Label exists lowercase",
+			currentLabels: []types.IssueLabel{
+				types.IssueLabel{
+					Name: "rod",
+				},
+				types.IssueLabel{
+					Name: "jane",
+				},
+				types.IssueLabel{
+					Name: "freddie",
+				},
+			},
+			cmdLabel:      "rod",
+			expectedFound: true,
+		},
+		{
+			title: "Label exists case insensitive",
+			currentLabels: []types.IssueLabel{
+				types.IssueLabel{
+					Name: "rod",
+				},
+				types.IssueLabel{
+					Name: "jane",
+				},
+				types.IssueLabel{
+					Name: "freddie",
+				},
+			},
+			cmdLabel:      "Rod",
+			expectedFound: true,
+		},
+		{
+			title: "Label doesnt exist lowercase",
+			currentLabels: []types.IssueLabel{
+				types.IssueLabel{
+					Name: "rod",
+				},
+				types.IssueLabel{
+					Name: "jane",
+				},
+				types.IssueLabel{
+					Name: "freddie",
+				},
+			},
+			cmdLabel:      "derek",
+			expectedFound: false,
+		},
+		{
+			title: "Label doesnt exist case insensitive",
+			currentLabels: []types.IssueLabel{
+				types.IssueLabel{
+					Name: "rod",
+				},
+				types.IssueLabel{
+					Name: "jane",
+				},
+				types.IssueLabel{
+					Name: "freddie",
+				},
+			},
+			cmdLabel:      "Derek",
+			expectedFound: false,
+		},
+		{
+			title:         "no existing labels lowercase",
+			currentLabels: nil,
+			cmdLabel:      "derek",
+			expectedFound: false,
+		},
+		{title: "Label doesnt exist case insensitive",
+			currentLabels: nil,
+			cmdLabel:      "Derek",
+			expectedFound: false,
+		},
+	}
+
+	for _, test := range stateOptions {
+		t.Run(test.title, func(t *testing.T) {
+
+			labelFound := findLabel(test.currentLabels, test.cmdLabel)
+
+			if labelFound != test.expectedFound {
+				t.Errorf("Find Labels(%s) - wanted: %t, got %t\n", test.title, test.expectedFound, labelFound)
+			}
+		})
+	}
+}
---
+++ permission-handler_test.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/miekg/dreck/types"
+)
+
+func Test_maintainersparsed(t *testing.T) {
+	config := types.DerekConfig{}
+	parseConfig([]byte(`maintainers:
+- alexellis
+- rgee0
+`), &config)
+	actual := len(config.Maintainers)
+	if actual != 2 {
+		t.Errorf("want: %d maintainers, got: %d", 2, actual)
+	}
+}
+
+func Test_curatorequalsmaintainer(t *testing.T) {
+	config := types.DerekConfig{}
+	parseConfig([]byte(`curators:
+- alexellis
+- rgee0
+`), &config)
+	actual := len(config.Maintainers)
+	if actual != 2 {
+		t.Errorf("want: %d maintainers, got: %d", 2, actual)
+	}
+}
+
+func TestEnabledFeature(t *testing.T) {
+
+	var enableFeatureOpts = []struct {
+		title            string
+		attemptedFeature string
+		configFeatures   []string
+		expectedVal      bool
+	}{
+		{
+			title:            "dco enabled try dco case sensitive",
+			attemptedFeature: "dco_check",
+			configFeatures:   []string{"dco_check"},
+			expectedVal:      true,
+		},
+		{
+			title:            "dco enabled try dco case insensitive",
+			attemptedFeature: "DCO_check",
+			configFeatures:   []string{"dco_check"},
+			expectedVal:      true,
+		},
+		{
+			title:            "dco enabled try comments",
+			attemptedFeature: "comments",
+			configFeatures:   []string{"dco_check"},
+			expectedVal:      false,
+		},
+		{
+			title:            "Comments enabled try comments case insensitive",
+			attemptedFeature: "Comments",
+			configFeatures:   []string{"comments"},
+			expectedVal:      true,
+		},
+		{
+			title:            "Comments enabled try comments case sensitive",
+			attemptedFeature: "comments",
+			configFeatures:   []string{"comments"},
+			expectedVal:      true,
+		},
+		{
+			title:            "Comments enabled try dco",
+			attemptedFeature: "dco",
+			configFeatures:   []string{"comments"},
+			expectedVal:      false,
+		},
+		{
+			title:            "Non-existent feature",
+			attemptedFeature: "gibberish",
+			configFeatures:   []string{"dco_check", "comments"},
+			expectedVal:      false,
+		},
+	}
+	for _, test := range enableFeatureOpts {
+		t.Run(test.title, func(t *testing.T) {
+
+			inputConfig := &types.DerekConfig{Features: test.configFeatures}
+
+			featureEnabled := enabledFeature(test.attemptedFeature, inputConfig)
+
+			if featureEnabled != test.expectedVal {
+				t.Errorf("Enabled feature - wanted: %t, found %t", test.expectedVal, featureEnabled)
+			}
+		})
+	}
+}
+
+func TestPermittedUserFeature(t *testing.T) {
+
+	var permittedUserFeatureOpts = []struct {
+		title            string
+		attemptedFeature string
+		user             string
+		config           types.DerekConfig
+		expectedVal      bool
+	}{
+		{
+			title:            "Valid feature with valid maintainer",
+			attemptedFeature: "comment",
+			user:             "Burt",
+			config: types.DerekConfig{
+				Features:    []string{"comment"},
+				Maintainers: []string{"Burt", "Tarquin", "Blanche"},
+			},
+			expectedVal: true,
+		},
+		{
+			title:            "Valid feature with valid maintainer case insensitive",
+			attemptedFeature: "comment",
+			user:             "burt",
+			config: types.DerekConfig{
+				Features:    []string{"comment"},
+				Maintainers: []string{"Burt", "Tarquin", "Blanche"},
+			},
+			expectedVal: true,
+		},
+		{
+			title:            "Valid feature with invalid maintainer",
+			attemptedFeature: "comment",
+			user:             "ernie",
+			config: types.DerekConfig{
+				Features:    []string{"comment"},
+				Maintainers: []string{"Burt", "Tarquin", "Blanche"},
+			},
+			expectedVal: false,
+		},
+		{
+			title:            "Valid feature with invalid maintainer case insensitive",
+			attemptedFeature: "Comment",
+			user:             "ernie",
+			config: types.DerekConfig{
+				Features:    []string{"comment"},
+				Maintainers: []string{"Burt", "Tarquin", "Blanche"},
+			},
+			expectedVal: false,
+		},
+		{
+			title:            "Invalid feature with valid maintainer",
+			attemptedFeature: "invalid",
+			user:             "Burt",
+			config: types.DerekConfig{
+				Features:    []string{"comment"},
+				Maintainers: []string{"Burt", "Tarquin", "Blanche"},
+			},
+			expectedVal: false,
+		},
+		{
+			title:            "Invalid feature with valid maintainer case insensitive",
+			attemptedFeature: "invalid",
+			user:             "burt",
+			config: types.DerekConfig{
+				Features:    []string{"comment"},
+				Maintainers: []string{"Burt", "Tarquin", "Blanche"},
+			},
+			expectedVal: false,
+		},
+	}
+
+	for _, test := range permittedUserFeatureOpts {
+		t.Run(test.title, func(t *testing.T) {
+
+			inputConfig := &test.config
+
+			permittedFeature := permittedUserFeature(test.attemptedFeature, inputConfig, test.user)
+
+			if permittedFeature != test.expectedVal {
+				t.Errorf("Permitted user feature - wanted: %t, found %t", test.expectedVal, permittedFeature)
+			}
+		})
+	}
+}
