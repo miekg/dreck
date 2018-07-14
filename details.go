996bcee3b51b39cc3386c19882d09d227601f8a9
Miek Gieben <miek@miek.nl>
2018-07-14 14:44:47 +0100

    slightly lower
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- details.go
+++ details.go
@@ -3,6 +3,7 @@ package dreck
 // Details contains the subscript that's get added to comments added by dreck.
 var Details = `
 
+
 <details>
 
 If you have questions or suggestions for this bot, please file an issue against the [miekg/dreck](https://github.com/miekg/dreck/issues/new?title=Dreck%20issue:) repository.
